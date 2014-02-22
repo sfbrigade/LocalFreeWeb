85f279b65379d61b55ccd71e9cd1b5c67a55c8cc
rcackerman <rebecca@cfa>
2014-02-22 12:47:08 -0800

    Updating name and adding testing.

--- testing/openfreeweb_tests.py
+++ testing/openfreeweb_tests.py
@@ -1,19 +1,19 @@
 import os
-import flaskr
+import openfreeweb
 import unittest
 import tempfile
 
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        flaskr.app.config['TESTING'] = True
-        self.app = flaskr.app.test_client()
-        flaskr.init_db()
+        self.db_fd, openfreeweb.app.config['DATABASE'] = tempfile.mkstemp()
+        openfreeweb.app.config['TESTING'] = True
+        self.app = openfreeweb.app.test_client()
+        openfreeweb.init_db()
 
     def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(flaskr.app.config['DATABASE'])
+        os.unlink(openfreeweb.app.config['DATABASE'])
 
 if __name__ == '__main__':
     unittest.main()
