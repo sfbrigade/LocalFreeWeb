a25627cd24909b2a3641991a2cd2bea7b8ac4000
Joseph Tanigawa <joseph.tanigawa@live.com>
2014-04-02 20:22:08 -0700

    Formatted text

--- LocalFreeWeb.py
+++ LocalFreeWeb.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
-#Enter bus stop id and it will print to console info for 3 closest locations
-#for free web
 
+#Sample response text message:
+#Ask for 'free internet' at these places: 4468 Mission St (10am-4pm); 4080 Mission St (930am-1230pm); 515 Cortland Ave (1230pm-1200am)
+
+#Sample initial text message:
+#Text 913*1 to 40909 for the nearest free internet access.
+#Texto 913*2 to 40909 para el internet mas proximo
+#Chinese 913*3 Chinese 40909 Chinese
+
+from flask import Flask, request, redirect
+import twilio.twiml
 import urllib, simplejson
 
 if __name__ == '__main__':
@@ -20,12 +28,13 @@ if __name__ == '__main__':
     get_closest_free_net_url = 'http://localfreeweb.cartodb.com/api/v2/sql?q=SELECT name, address, zip, phone, ST_Distance(the_geom::geography, ST_PointFromText(\'POINT('+ geo_long + ' ' + geo_lat + ')\', 4326)::geography) AS distance FROM freeweb ORDER BY distance ASC LIMIT 3'
     response = urllib.urlopen(get_closest_free_net_url)
     for line in response:
-    	response_dict = simplejson.loads(line)
+       	response_dict = simplejson.loads(line)
 #    	print response_dict
-    	for i in range(0, 3):
-    	    print '\nResult ' + str(i + 1) + ': '
-    	    print str(response_dict['rows'][i]['name'])
-    	    print str(response_dict['rows'][i]['address'])
-    	    print 'San Francisco, CA ' + str(response_dict['rows'][i]['zip'])
-    	    print 'Phone number: ' + str(response_dict['rows'][i]['phone'])
+        response = '\nAsk for \'free internet\' at these places:'
+    for i in range(0, 3):
+#    	    print str(response_dict['rows'][i]['name'])
+    	    response += ' ' + str(response_dict['rows'][i]['address']) + ';'
+#    	    print 'San Francisco, CA ' + str(response_dict['rows'][i]['zip'])
+#    	    print 'Phone number: ' + str(response_dict['rows'][i]['phone'])
+    print response
     	    
